45015e2124d82ff9839aff51a60b4a5425fdd89e
Ian Eddy <ianmseddy@gmail.com>
2023-07-05 09:05:19 -0700

    gg map function

--- summary_plots.R
+++ summary_plots.R
@@ -3,82 +3,93 @@ library(sf)
 library(ggplot2)
 library(reproducible)
 
-rangePolys <- st_read("gis/Digitized_Caribou_StudyAreas.shp")
-rangePolys$Id <- NULL
+RangePolygons <- st_read("gis/Digitized_Caribou_StudyAreas.shp")
+RangePolygons <- st_make_valid(RangePolygons) #Mahoney80_MiddleRinge is invalid..?
+RangePolygons$Id <- NULL
 Canada <- prepInputs(url = paste0("https://www12.statcan.gc.ca/census-recensement/2011/",
                                   "geo/bound-limit/files-fichiers/2016/lpr_000b16a_e.zip"),
                      destinationPath = "GIS",
                      fun = "st_read")
 #actually since the polygons are equal area... use somethign else
-Canada <- st_transform(Canada, st_crs(rangePolys))
-Canada <- st_crop(Canada, y = st_bbox(st_buffer(rangePolys, 30000)))
+Canada <- st_transform(Canada, st_crs(RangePolygons))
+Canada <- st_crop(Canada, y = st_bbox(st_buffer(RangePolygons, 30000)))
 #multiplying the bbox doesn't work because both ys are positive
 
-rangeData_postGIS <- fread("outputs/Caribou_Range_Disturbance_Summary.csv")
-rangeData_postGIS[, V1 := NULL]
+landscapeStats <- fread("outputs/Caribou_Range_Disturbance_Summary.csv")
+landscapeStats[, V1 := NULL]
 
-rangePolys1 <- dplyr::left_join(rangePolys, rangeData_postGIS, "PolygonID")
-demoData <- fread("Range_Polygon_Data.csv")
-demoData[, c("V1", "Note") := NULL]
-demoData[, CalfCow := as.numeric(CalfCow)] #TODO correct this properly
-demoData[CalfCow > 1.5, CalfCow := CalfCow/100]
-demoData[, c("DocID", "Author") := NULL] #some authors were mismatched due to periods at end of et al
+RangePolygons <- dplyr::left_join(RangePolygons, landscapeStats, "PolygonID")
+demographicData <- fread("Range_Polygon_Data.csv")
+demographicData[, c("V1", "Note") := NULL]
+demographicData[, CalfCow := as.numeric(CalfCow)] #TODO correct this properly
+demographicData[CalfCow > 1.5, CalfCow := CalfCow/100]
+demographicData[, c("DocID", "Author") := NULL] #some authors were mismatched due to periods at end of et al
 
-rangePolys1 <- dplyr::left_join(rangePolys1, demoData, c("PolygonID"))
+RangePolygons <- dplyr::left_join(RangePolygons, demographicData, c("PolygonID"))
+
+##NOTE: none of the disturbance rates are adjusted for habitat area yet
+# so harvest rates are lower in BC due to presence of non-forest
+
+#TODO: pctVeg and pctForest are still proportions..
+if (max(RangePolygons$pctForest) < 50){
+  RangePolygons$pctForest <- RangePolygons$pctForest * 100
+  RangePolygons$pctVeg <- RangePolygons$pctVeg * 100
+}
+
+#maps
+makeMapGG <- function(df = RangePolygons, CA = Canada, stat, adjustCol = NULL,
+                      fillLab, outputFilename) {
+  df$statOfInterest <- df[stat][[1]]
+  if (!is.null(adjustCol)){
+    df$statOfInterest <- df$statOfInterest/df[adjustCol][[1]] * 100
+  }
+  
+  mygg <- ggplot(df) + 
+    geom_sf(data = CA, show.legend = FALSE) +
+    geom_sf(data = df, aes(fill = statOfInterest), alpha = 0.7) + 
+    scale_fill_continuous(type = "viridis") + 
+    labs(fill = fillLab) + 
+    guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
+    theme_bw() + 
+    theme(legend.text=element_text(size=rel(1.2)), 
+          legend.title=element_text(size=rel(1.2)))
+  ggsave(mygg, filename = outputFilename, device = "png", height = 6, width = 15)
+}
 
 
-out1 <- ggplot(rangePolys1) + 
+###harvest rate
+makeMapGG(stat = "pctHarvestYr", 
+          fillLab = "harvest rate (%/year) \n1985 - time of study",
+          outputFilename = "figures/pctHarvestYr_gg.png")
+
+makeMapGG(stat = "pctHarvestYr", adjustCol = "pctVeg", 
+          fillLab = "harvest rate \n(% of habitat/year) \n1985 - time of study",
+          outputFilename = "figures/pctHarvestYrAdj_gg.png")
+#to discuss with Eric - but we should probably adjust for the amount of habitat
+makeMapGG(stat = "pctDisturbedYr",
+          fillLab = "disturbance rate (%/year): \n1985 - time of study",
+          outputFilename = "figures/pctDistYr_gg.png")
+
+makeMapGG(stat = "pctDisturbedYr", adjustCol = "pctVeg", 
+          fillLab = "disturbance rate \n(% of habitat/year) \n1985 - time of study",
+          outputFilename = "figures/pctDistYrAdj_gg.png")
+
+#burn
+makeMapGG(stat = "pctBrnYr",
+          fillLab = "burn rate (%/year): \n1985 - time of study",
+          outputFilename = "figures/pctBrnYr_gg.png")
+
+makeMapGG(stat = "pctBrnYr", adjustCol = "pctVeg", 
+          fillLab = "burn rate \n(% of habitat/year) \n1985 - time of study",
+          outputFilename = "figures/pctBrnAdj_gg.png")
+
+#demographic 
+makeMapGG(stat = "pctVeg", fillLab = 'potential habitat \nin polygon (%)',
+          outputFilename = "figures/pctVeg_gg.png")
+
+out5 <- ggplot(RangePolygons) + 
   geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = propHarvest), alpha = 0.5) + 
-  scale_fill_continuous(type = "viridis") + 
-  labs(fill = "proportion harvested: \n 1985 - time of study") + 
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
-  theme_bw() + 
-  theme(legend.text=element_text(size=rel(1.2)), 
-        legend.title=element_text(size=rel(1.2)))
-ggsave(plot = out1, filename = "figures/propHarvest_gg.png",
-       device = "png", , height = 6, width = 15)
-
-out2 <- ggplot(rangePolys1) + 
-  geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = propDisturbed), alpha = 0.5) + 
-  scale_fill_continuous(type = "viridis") + 
-  labs(fill = "proportion disturbed: \n1985 - time of study") + 
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
-  theme_bw() + 
-  theme(legend.text=element_text(size=rel(1.2)), 
-        legend.title=element_text(size=rel(1.2)))
-ggsave(plot = out2, filename = "figures/propDist_gg.png", 
-       device = "png", , height = 6, width = 15)
-
-out3 <- ggplot(rangePolys1) + 
-  geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = propBurned), alpha = 0.5) + 
-  scale_fill_continuous(type = "viridis") + 
-  labs(fill = "proportion burned: \n 1985 - time of study") + 
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
-  theme_bw() + 
-  theme(legend.text = element_text(size=rel(1.2)), 
-        legend.title = element_text(size=rel(1.2)))
-ggsave(plot = out3, filename = "figures/propBurned_gg.png", 
-       device = "png", , height = 6, width = 15)
-
-
-out4 <- ggplot(rangePolys1) + 
-  geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = Meas_Years), alpha = 0.5) + 
-  scale_fill_continuous(type = "viridis") + 
-  labs(fill = "last year\n of study") + 
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
-  theme_bw() + 
-  theme(legend.text=element_text(size=rel(1.2)), 
-        legend.title=element_text(size=rel(1.2)))
-ggsave(plot = out4, filename = "figures/measYears_gg.png", 
-       device = "png", , height = 6, width = 15)
-
-out5 <- ggplot(rangePolys1) + 
-  geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = as.numeric(Lambda)), alpha = 0.5) + 
+  geom_sf(data = RangePolygons, aes(fill = as.numeric(Lambda)), alpha = 0.5) + 
   scale_fill_continuous(type = "viridis") + 
   labs(fill = "lambda") + 
   guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
@@ -88,9 +99,9 @@ out5 <- ggplot(rangePolys1) +
 ggsave(plot = out5, filename = "figures/Lambda_gg.png", 
        device = "png", , height = 6, width = 15)
 
-out6 <- ggplot(rangePolys1) + 
+out6 <- ggplot(RangePolygons) + 
   geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = as.numeric(CalfCow)), alpha = 0.5) + 
+  geom_sf(data = RangePolygons, aes(fill = as.numeric(CalfCow)), alpha = 0.5) + 
   scale_fill_continuous(type = "viridis") + 
   labs(fill = "Calf:Cow") + 
   guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
@@ -100,9 +111,9 @@ out6 <- ggplot(rangePolys1) +
 ggsave(plot = out6, filename = "figures/CalfCow_gg.png", 
        device = "png", , height = 6, width = 15)
 
-out7 <- ggplot(rangePolys1) + 
+out7 <- ggplot(RangePolygons) + 
   geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = as.numeric(AdultFemaleSurvivalRate)), alpha = 0.5) + 
+  geom_sf(data = RangePolygons, aes(fill = as.numeric(AdultFemaleSurvivalRate)), alpha = 0.5) + 
   scale_fill_continuous(type = "viridis") + 
   labs(fill = "Adult Female \nSurvival Rate") + 
   guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
@@ -112,9 +123,9 @@ out7 <- ggplot(rangePolys1) +
 ggsave(plot = out7, filename = "figures/AdultFemaleSurvivalRate_gg.png", 
        device = "png", , height = 6, width = 15)
 
-out8 <- ggplot(rangePolys1) + 
+out8 <- ggplot(RangePolygons) + 
   geom_sf(data = Canada, show.legend = FALSE) +
-  geom_sf(data = rangePolys1, aes(fill = as.numeric(meanMag)), alpha = 0.5) + 
+  geom_sf(data = RangePolygons, aes(fill = as.numeric(meanMag)), alpha = 0.5) + 
   scale_fill_continuous(type = "viridis") + 
   labs(fill = "Mean Disturbance \nMagnitude") +
   guides(colour = guide_legend(override.aes = list(alpha = 1))) + 
@@ -124,13 +135,15 @@ out8 <- ggplot(rangePolys1) +
 ggsave(plot = out8, filename = "figures/meanMag_gg.png", 
        device = "png", height = 6, width = 15)
 
-outputTable <- as.data.table(rangePolys1) 
+outputTable <- as.data.table(RangePolygons) 
 outputTable <- outputTable[, .(DocID, PolygonID, Province, 
                                `First Measurement Year`, `Last Measurement Year`,
                                Lambda, AdultFemaleSurvivalRate, CalfCow, Pregnancy,
-                               propDisturbed, propHarvest, propBurned, meanMag,
+                               pctDisturbedYr, pctHarvestYr, pctBrnYr, meanMag,
                                propForest, propVeg, `Predator Control`)]
-toRound <- c("propDisturbed", "propForest", "propVeg", "meanMag", "propHarvest", "propBurned")
+
+#so far this is only for illustrative purposes, hence rounding
+toRound <- c("pctDisturbedYr", "propForest", "propVeg", "meanMag", "pctHarvestYr", "pctBrnYr")
 outputTable <- unique(outputTable) #unique b/c of multipolygons
 outputTable[, (toRound) := lapply(.SD, round, digits = 3), .SDcol = toRound]
 outputTable[, meanMag := round(meanMag, digits = 0)]
@@ -139,8 +152,8 @@ setnames(outputTable,
                  "First Measurement Year",
                  "Last Measurement Year", 
                  "Pregnancy", "CalfCow",
-                 "meanMag", "propDisturbed", "propBurned",
-                 "propHarvest", "propForest", "propVeg",
+                 "meanMag", "pctDisturbedYr", "pctBrnYr",
+                 "pctHarvestYr", "propForest", "propVeg",
                  "Predator Control"),
          new = c("adult female surv. rate",
                  "first msr. year",
