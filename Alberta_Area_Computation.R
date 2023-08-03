e3b61773f40c22f008d883b29f55691e111f81fd
Ian Eddy <ianmseddy@gmail.com>
2023-08-03 16:37:53 -0700

    finish Alberta work

--- Alberta_Area_Computation.R
+++ Alberta_Area_Computation.R
@@ -3,9 +3,9 @@ library(terra)
 library(sf)
 library(magrittr)
 library(reproducible)
-
+library(data.table)
 #don't use the prepackaged AOI because it assumes every image is used, even those with cloudcover > 25%
-
+#1) build complete AOI from shapefiles
 rsShape <- list.files("GIS/Linear_Features/RS imagery/Dalerum_StuartSmith", 
                       recursive = TRUE, pattern = ".shp", full.names = TRUE)
 rsShape <- rsShape[grep("aoi", rsShape, invert = TRUE)]
@@ -15,7 +15,9 @@ rsShape <- lapply(rsShape, st_read) %>%
   st_union(by_feature = FALSE) %>%
   vect()
 
-# writeVector(rsShape, "outputs/corrected_Alberta_Polygons/OrbView_Extent.shp", overwrite = TRUE)
+writeVector(rsShape, "outputs/corrected_Alberta_Polygons/OrbView_Extent.shp", overwrite = TRUE)
+
+#2) crop linear features to each
 rangePolys <- vect("GIS/Digitized_Caribou_StudyAreas.shp")
 polyIDs <- c("StuartSmith79_WSAR", "StuartSmith79_ESAR", 
              "Dalerum67_ESAR", "Dalerum67_RedEarth", "Dalerum67_CaribouMountains")
@@ -23,31 +25,43 @@ polyIDs <- c("StuartSmith79_WSAR", "StuartSmith79_ESAR",
 # rangePolyArea <- expanse(rangePolys)
 
 #crop the linear features to the extent of the OrbView3 imagery for faster digitization 
+sapply(polyIDs, FUN = function(x){
+  origLines <- vect(paste0("outputs/linear_features/", x, "_linear_features.shp"))
+  cropped <- postProcess(origLines, cropTo = rsShape, maskTo = rsShape)
+  #use postProcess as terra::mask does not crack features
+  if (nrow(cropped) != 0){
+    writeVector(cropped, paste0("outputs/corrected_Alberta_Polygons/", x, "_linear_features_past.shp"),
+                overwrite = TRUE)
+  }
+})
 
-if (FALSE) {
-  sapply(polyIDs, FUN = function(x){
-    origLines <- vect(paste0("outputs/linear_features/", x, "_linear_features.shp"))
-    cropped <- postProcess(origLines, cropTo = rsShape, maskTo = rsShape)
-    #use postProcess as terra::mask does not crack features
-    if (nrow(cropped) != 0){
-      writeVector(cropped, paste0("outputs/corrected_Alberta_Polygons/", x, "_linear_features_past.shp"),
-                  overwrite = TRUE)
-    }
-  })
-}
+#after removing lines that were not present in the 2005/2006 year, files were renamed with "corrected" suffix
 
+#3 calculate the area covered
 #first, project OrvView3 extents to Range polygons
-# polys <- project(polys, rangePolys)
 # #then mask them to the relevant polygon - only StuartSmith_WSAR is covered at the moment
-# polys <- mask(polys, mask = rangePolys)
-# OrbView3Area_1 <- expanse(polys, transform = TRUE)
-# names(rangePolyArea) <- rangePolys$PolygonID
-# #as a percentage
-# OrbView3Area_1/rangePolyArea["StuartSmith79_WSAR"] * 100 
-temp <- vect("outputs/corrected_Alberta_Polygons/StuartSmith79_ESAR_linear_features_past.shp")
-dalerum <- rangePolys[grep("Dalerum67_ESAR", rangePolys$PolygonID),]
-dalerum <- project(dalerum, y = temp)
-temp <- terra::mask(temp, dalerum, inverse = TRUE)
-plot(temp)
-writeVector(temp, "StuartSmith79_ESAR_linear_features_past.shp", overwrite = TRUE)
-#crop the original lines with the Dalerum polygon
+rsShape <- project(rsShape, rangePolys)
+areaCovered <- lapply(polyIDs, function(x, OrbView = rsShape){
+  studyArea <- rangePolys[rangePolys$PolygonID == x,]
+  OrbView <- postProcess(OrbView, cropTo = studyArea, maskTo = studyArea)
+  areaCovered <- sum(expanse(OrbView, transform = TRUE))
+  totalArea <- sum(expanse(studyArea, transform = TRUE))
+  return(data.table(PolygonID = x, percentCovered = areaCovered/totalArea * 100))
+})
+areaCovered <- rbindlist(areaCovered)
+
+#calculate the new and original lines
+correctedLinearFootprint <- function(x){
+  originalLines <- vect(paste0("outputs/corrected_Alberta_Polygons/", 
+                               x, "_linear_features_past.shp"))
+  correctedLines <- vect(paste0("outputs/corrected_Alberta_Polygons/",
+                                x, "_linear_features_corrected.shp"))
+  correctedLines$length <- terra::perim(correctedLines)
+  originalLines$length <- terra::perim(originalLines)
+  
+  return(data.table(PolygonID = x, 
+                    oldLength = sum(originalLines$length),
+                    newLength = sum(correctedLines$length),
+                    measureYear = unique(correctedLines$MeasureYear)))
+  
+}
